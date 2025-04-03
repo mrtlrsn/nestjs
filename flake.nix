40f015459d7368dfa00e0887d715434ab2a124dd
Martin Larsson <martin.larsson.89@gmail.com>
2025-04-03 13:58:41 +0200

    New nestjs project

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A development environment with Node.js for Tibber case";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_22
+          ];
+
+          shellHook = ''
+            echo "Node.js development environment activated!"
+            echo "Node.js version: $(node --version)"
+            echo "npm version: $(npm --version)"
+          '';
+        };
+      }
+    );
+}
